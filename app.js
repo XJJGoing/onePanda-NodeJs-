ddab2cc05f878761f4f714a6950f2a28c426c4e7
XJJGoing <1499755237@qq.com>
2019-03-06 17:33:18 +0800

    update

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-//我是server端
+/*入口文件的配置*/
+const express = require('express');
+const fs = require('fs');
+const app =express();
+
+
+
+app.listen(3000,()=>{
+   console.log("running")
+});
---
+++ models/user.js
@@ -0,0 +1,5 @@
+require('../connect/connect');
+const mongoose = require('mongoose');
+let userSchema = mongoose.Schema({
+     name:{}
+});

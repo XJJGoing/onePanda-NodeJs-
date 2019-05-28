dda737c2e7db0eb43e3baa89ec92845c0a247494
XJJ <43441833+XJJGoing@users.noreply.github.com>
2019-05-28 09:30:22 +0800

    Update connect.js

--- connect/connect.js
+++ connect/connect.js
@@ -1,6 +1,6 @@
 /*连接数据库的模块*/
 const mongooose = require('mongoose');
-mongooose.connect('mongodb://czh:qiangg1235@127.0.0.1:27017/JustReading',{useNewUrlParser:true});
+mongooose.connect('mongodb://xxx:xxxx@127.0.0.1:27017/JustReading',{useNewUrlParser:true});
 mongooose.connection.once('open',()=> {
     console.log('数据库连接成功')
 });
